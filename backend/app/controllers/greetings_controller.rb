3bd8c5b2daad7cdff67d8f760ff3386aca98f511
Westen0511 <xiyuanjunzhi@gmail.com>
2022-10-16 15:58:40 +0900

    test: Test pre-commit

--- backend/app/controllers/greetings_controller.rb
+++ backend/app/controllers/greetings_controller.rb
@@ -1,5 +1,5 @@
 class GreetingsController < ApplicationController
   def hello
-    render json: { content: 'Hello from Rails' }
+    render json: { content: 'Hello from Rails by Docker.' }
   end
 end
